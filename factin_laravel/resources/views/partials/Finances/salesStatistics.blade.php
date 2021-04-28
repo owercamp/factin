22603499b0a16de4fe81be487b0aa67c9f0fc015
ower armando campos alfonso <owerion22@gmail.com>
2021-04-27 22:21:55 -0500

    se configurala destroy() para reutilizar la etiqueta canvas al cambiar de seleccion de mi año

--- factin_laravel/resources/views/partials/Finances/salesStatistics.blade.php
+++ factin_laravel/resources/views/partials/Finances/salesStatistics.blade.php
@@ -2,7 +2,7 @@
 @extends('modules.settingFinance')
 
 @section('info')
-    <div class="w-100">
+    <div class="w-100">        
         @php
             $yearnow = date('Y');
 			$mountnow = date('m');
@@ -19,13 +19,122 @@
 			<option value="{{ $yearfutureTwo }}">{{ $yearfutureTwo }}</option>
         </select>
     </div>
+    <div class="w-100 d-flex justify-content-center">
+        <div class="point-loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
     <div class="border-info container-fluid" style="width: 900px !important; height: 500px !important">
-        <canvas id="MyStatistics" class="border-secondary border p-4"></canvas>
+        <canvas id="MyStatistics" class="p-4 myCanvas"></canvas>
     </div>
 @endsection
 
 @section('ScriptZone')    
-    <script>        
+    <script>
+        let chart;
+        // al realizar el cambio de mi año
+        $('select[name=yearSelected]').change(function (e) { 
+            e.preventDefault();
+            $('.point-loader').fadeIn();
+            $('.myCanvas').fadeOut();
+            if(chart){
+                chart.destroy();
+            }
+            let year = $('select[name=yearSelected]').val();
+            let Arr = [];
+            $.getJSON("{{route('getChartSales')}}", {data: year},
+            function (objectDataMonth) {
+                let Mes = {'Enero': 0,'Febrero': 0,'Marzo': 0,'Abril': 0,'Mayo': 0,'Junio': 0,'Julio': 0,'Agosto': 0,'Septiembre': 0,'Octubre': 0,'Noviembre': 0,'Diciembre': 0};
+                for (const key in Mes) {
+                    for (const iterator of objectDataMonth) {
+                        let month = iterator['bo_month'];
+                        let value = iterator['bo_sale_month'];
+                        if(key == month)
+                        {
+                            Mes[key] = value;
+                        }
+                    }
+                } 
+                for (const key in Mes) {
+                    Arr.push(Mes[key]);
+                }           
+                function sale(info){            
+                let Month = [ 'Enero', 'Febrero', 'Marzo', 'Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+                    chart = new Chart(info,{
+                        type:'line',
+                        data: {
+                            labels: Month,
+                            datasets: [
+                                {
+                                    label:'Ventas',
+                                    data: Arr,
+                                    borderColor: '#007bff',
+                                    backgroundColor: '#007bff8a',
+                                    tension: 0.3,
+                                }
+                            ]
+                        },
+                        options: {
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'VENTAS DEL AÑO '+year,
+                                    padding: 20,
+                                    color: '#007bff8a',
+                                    font: {
+                                        weight: 'bold',
+                                        size: 30,
+                                        family: 'Helvetica',
+                                    }
+                                },
+                                legend: {
+                                    position: 'bottom',
+                                    labels: {
+                                        padding: 20
+                                    }
+                                },
+                                tooltip: {
+                                    backgroundColor: '#0584f6',
+                                    titleFont: {
+                                        size: 13,
+                                    },
+                                    padding: 8,
+                                    bodyFont: {
+                                        size: 12,
+                                    },
+                                }                            
+                            },
+                            elements: {
+                                point: {
+                                    pointStyle: 'star',
+                                    radius: 5,
+                                    hoverRadius: 8,
+                                },
+                                line: {
+                                    borderWidth: 1.5,
+                                    borderColor: 'black',
+                                    fill: true,
+                                }
+                            }
+                        },
+                    });
+                }
+                function renders() {
+                    const info = document.querySelector('#MyStatistics').getContext("2d");
+                    sale(info);
+                }
+                renders();
+                } 
+            );
+            $('.myCanvas').fadeIn(2300);
+            $('.point-loader').fadeOut(2800);            
+            
+        });
+
+        // consulta inicial al cargar mi pages    
         let year = $('select[name=yearSelected]').val();
         let Arr = [];
         $.getJSON("{{route('getChartSales')}}", {data: year},
@@ -46,7 +155,7 @@
             }           
             function sales(ctx){            
             let Month = [ 'Enero', 'Febrero', 'Marzo', 'Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
-                const chart = new Chart(ctx,{
+                chart = new Chart(ctx,{
                     type:'line',
                     data: {
                         labels: Month,
@@ -60,18 +169,49 @@
                             }
                         ]
                     },
-                    options:{
-                        title:{
-                            display: true,
-                            text: 'VENTAS DEL AÑO',
-                            fontSize: 20,
-                            padding: 20,
-                            fontColor: '#6c757d',
+                    options: {
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: 'VENTAS DEL AÑO '+year,
+                                padding: 20,
+                                color: '#007bff8a',
+                                font: {
+                                    weight: 'bold',
+                                    size: 30,
+                                    family: 'Helvetica',
+                                }
+                            },
+                            legend: {
+                                position: 'bottom',
+                                labels: {
+                                    padding: 20
+                                }
+                            },
+                            tooltip: {
+                                backgroundColor: '#0584f6',
+                                titleFont: {
+                                    size: 13,
+                                },
+                                padding: 8,
+                                bodyFont: {
+                                    size: 12,
+                                },
+                            }                            
+                        },
+                        elements: {
+                            point: {
+                                pointStyle: 'star',
+                                radius: 5,
+                                hoverRadius: 8,
+                            },
+                            line: {
+                                borderWidth: 1.5,
+                                borderColor: 'black',
+                                fill: true,
+                            }
                         }
                     },
-                    legend:{
-                        position: 'bottom',
-                    }
                 });
             }
             function render() {
@@ -81,5 +221,7 @@
             render();
             } 
         );
+        $('.myCanvas').fadeIn(2300);
+        $('.point-loader').fadeOut(2800);
     </script>
 @endsection

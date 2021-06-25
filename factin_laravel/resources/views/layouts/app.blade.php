5d5d827fe7642d2343012b74880749a68c1a736b
ower armando campos alfonso <owerion22@gmail.com>
2021-06-24 22:22:56 -0500

    se comprimen los archivos JS y CSS que se encontraba en Dev

--- factin_laravel/resources/views/layouts/app.blade.php
+++ factin_laravel/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Styles -->
-    <link rel="Icon" href="{{asset('img/logofactin.png')}}">
+    <link rel="Icon" href="{{asset('img/logofactin.png')}}">    
     <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/mtstyle.css')}}">
